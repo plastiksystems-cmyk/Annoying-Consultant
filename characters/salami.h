2cc0921aa81f85c14a82d23f7e0bc42d3c717787
highspeedtrain <hst@highspeedtrain.net>
2026-04-27 13:42:20 +0100

    what

---
+++ characters/salami.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "cscharacter.h"
+
+namespace cs {
+    inline DLL_EXPORT character salami{"salami", 1.5, {
+        "ai",
+        "copilot",
+        "i must vibecode html and css",
+        "ai battlepass",
+        "please buy me more copilot credits",
+        "chatgpt"
+    }};
+}
